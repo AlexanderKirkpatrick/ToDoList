2170c15d37daeeeac59a6976bc34a4d0c8870d95
Alexander <knibz@protonmail.com>
2022-06-26 15:57:03 -0400

    add initial files

---
+++ ToDoList.Solution/ToDoList.Test/ModelTests/ItemTests.cs
@@ -0,0 +1,57 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using ToDoList.Models;
+
+namespace ToDoList.Tests
+{
+  [TestClass]
+    public class ItemTest : IDisposable 
+  {
+
+    public void Dispose()
+      {
+        Item.ClearAll();
+      }
+  
+    
+    [TestMethod]
+    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    {
+      Item newItem = new Item("test");
+      Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+      string result = newItem.Description;
+      Assert.AreEqual(description, result);
+    }
+
+    [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+      string updatedDescription = "Do the dishes";
+      newItem.Description = updatedDescription;
+      string result = newItem.Description;
+      Assert.AreEqual(updatedDescription, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_ItemList()
+    {
+      List<Item> newList = new List<Item> {};
+      List<Item> result = Item.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+  }
+
+}
+
