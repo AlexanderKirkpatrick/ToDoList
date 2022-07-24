cb58e019c0dfd51c6446cd58e6c63e18b67743d1
Alexander <knibz@protonmail.com>
2022-07-24 15:10:14 -0400

    add home links to views pages, edit mySQL table to be Int instead of varChar

--- ToDoList.Solution/ToDoList/Controllers/ItemsController.cs
+++ ToDoList.Solution/ToDoList/Controllers/ItemsController.cs
@@ -1,8 +1,9 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.EntityFrameworkCore;
 
 namespace ToDoList.Controllers
 {
